4cd51e3512be79851bc6164a47df1b99e4ab2204
Back2VN <80081864+Back2VN@users.noreply.github.com>
2024-12-31 06:18:03 +0700

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,10 +10,13 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
+# Thiết lập logging mức INFO (bạn có thể thay bằng DEBUG nếu muốn chi tiết hơn)
+app.logger.setLevel(logging.INFO)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-import FlaskWebProject.views
+import FlaskWebProject.views  # Phải import sau khi app được tạo
