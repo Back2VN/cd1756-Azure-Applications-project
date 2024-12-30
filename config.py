203beeacdc08e458c1fcaec632b9f1ee9abbb082
Back2VN <80081864+Back2VN@users.noreply.github.com>
2024-12-31 06:17:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
@@ -13,29 +12,41 @@ class Config(object):
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
-    # Below URI may need some adjustments for driver version, based on your OS, if running locally
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
+
+    # Kết nối DB
+    SQLALCHEMY_DATABASE_URI = (
+        'mssql+pyodbc://'
+        + SQL_USER_NAME
+        + '@'
+        + SQL_SERVER
+        + ':'
+        + SQL_PASSWORD
+        + '@'
+        + SQL_SERVER
+        + ':1433/'
+        + SQL_DATABASE
+        + '?driver=ODBC+Driver+17+for+SQL+Server'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
-    ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
-    # In your production app, Microsoft recommends you to use other ways to store your secret,
-    # such as KeyVault, or environment variable as described in Flask's documentation here:
-    # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-    # if not CLIENT_SECRET:
-    #     raise ValueError("Need to define CLIENT_SECRET environment variable")
+    # Đọc từ biến môi trường trước, nếu chưa có thì lấy chuỗi mặc định
+    CLIENT_ID = os.getenv("CLIENT_ID") or "ENTER_CLIENT_ID_HERE"
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET") or "ENTER_CLIENT_SECRET_HERE"
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
-    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+    # Nếu bạn có TENANT_ID trong biến môi trường, ghép vào authority
+    # Nếu không có, dùng "common" (multi-tenant).
+    TENANT_ID = os.getenv("TENANT_ID")
+    if TENANT_ID:
+        AUTHORITY = "https://login.microsoftonline.com/" + TENANT_ID
+    else:
+        AUTHORITY = "https://login.microsoftonline.com/common"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    # Đường dẫn callback, khớp với @app.route(Config.REDIRECT_PATH) trong views.py
+    REDIRECT_PATH = "/getAToken"
 
-    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+    # Scopes - tuỳ bạn có cần scope nào khác, mặc định "User.Read"
+    SCOPE = ["User.Read"]
 
-    # You can find the proper permission names from this document
-    # https://docs.microsoft.com/en-us/graph/permissions-reference
-    SCOPE = ["User.Read"] # Only need to read user profile for this app
-
-    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
+    # Lưu token MSAL trong session server-side
+    SESSION_TYPE = "filesystem"
